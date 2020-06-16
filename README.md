ec3f9f239c4847d02eefeeb30c7288f14bc370db
cbjun <1656043947@qq.com>
2020-06-16 15:47:22 +0800

    修改

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SpringAOP
 1、aop简单实例
 2、jdk动态代理和cilgb动态代理实例
+3、webservice的调用
+    3.1、获取wsdl网址，在浏览器上另存为wsdl文件
+    3.2、通过SoapUI解析入参格式
+    3.3、通过该项目中WeatherWebservice中代码请求
--- spring-helloworld/src/main/java/com/example/test/Test02.java
+++ spring-helloworld/src/main/java/com/example/test/Test02.java
@@ -2,8 +2,21 @@ package com.example.test;
 
 public class Test02 {
     public static void main(String[] args) {
-        Object c = "b";
-        char a = c.toString().toCharArray()[0];
-        System.out.println(a);
+        StringBuffer sb = new StringBuffer("");
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        sb.append("<soapenv:Envelope "
+                + "    xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' "
+                + " xmlns:q0='http://WebXml.com.cn/' "
+                + "    xmlns:xsd='http://www.w3.org/2001/XMLSchema' "
+                + " xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' >");
+        sb.append("<soapenv:Body>");
+        sb.append("<q0:getWeatherbyCityName>");
+        sb.append("<q0:theCityName>唐山</q0:theCityName> ");
+        sb.append("</q0:getWeatherbyCityName>");
+        sb.append("</soapenv:Body>");
+        sb.append("</soapenv:Envelope>");
+        System.out.println(sb.toString());
+        System.out.println("aaa");
     }
+
 }
