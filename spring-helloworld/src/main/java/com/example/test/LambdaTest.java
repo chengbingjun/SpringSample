a827342fb4e50b09ea71eff40f237ee463fe137b
cbjun <1656043947@qq.com>
2020-06-29 16:13:04 +0800

    lambda表达式的内容添加以及复习

--- spring-helloworld/src/main/java/com/example/test/LambdaTest.java
+++ spring-helloworld/src/main/java/com/example/test/LambdaTest.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import com.google.common.collect.Lists;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -16,16 +17,67 @@ public class LambdaTest {
         strings.add("憨憨");
         List<String> stringList = Lists.newArrayList();
         strings.forEach(e -> {
-            if(e.length()>2){
-                stringList.add(0,e);
+            if (e.length() > 2) {
+                stringList.add(0, e);
             }
         });
         System.out.println(stringList.size());
+        stringList.clear();
+        System.out.println(stringList.size());
 
-        Runnable runnable1 = () ->{
-            System.out.println("线程一启动");
+        //双冒号的用法：实例对象调用了add方法
+        strings.forEach(stringList::add);
+
+        System.out.println(stringList.size());
+
+        /**
+         * Lambda 表达式需要 “函数式接口” 的支持
+         * 函数式接口 : 接口中只有一个抽象方法的接口，称为函数式接口，可以通过 Lambda 表达式来创建该接口的对象
+         * (若 Lambda表达式抛出一个受检异常，那么该异常需要在目标接口的抽象方法上进行声明) 可
+         * 以使用注解 @FunctionalInterface 修饰可以检查是否是函数式接口，
+         */
+        Runnable runnable1 = () -> {
+            //测试着玩的 start->
+            int i = 1 + 1;
+            // <-end
+            System.out.println("线程一启动" + i);
         };
         Thread thread1 = new Thread(runnable1);
         thread1.start();
+
+
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                //相当于
+                int i = 1 + 1;
+                System.out.println("线程二启动" + i);
+            }
+        };
+
+        Thread thread2 = new Thread(runnable2);
+        thread2.start();
+        List<Integer> listA = Lists.newArrayList();
+        listA.add(1);
+        listA.add(2);
+        listA.add(0);
+        listA.add(6);
+        listA.add(5);
+
+        Comparator<Integer> comparator1 = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        Comparator<Integer> comparator2 = (o1,o2)->{
+            return o1.compareTo(o2);
+        };
+
+        listA.sort(comparator2);
+        listA.forEach(e->{
+            System.out.println(e);
+        });
     }
 }
