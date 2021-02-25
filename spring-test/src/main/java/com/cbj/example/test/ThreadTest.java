abcc9673386f78a2c3e98d090df430b595e8a4a0
cbjun <1656043947@qq.com>
2021-02-25 13:58:34 +0800

    创建线程的三种方法

---
+++ spring-test/src/main/java/com/cbj/example/test/ThreadTest.java
@@ -0,0 +1,45 @@
+package com.cbj.example.test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @Author cbjun
+ * @create 2021/2/25 11:19
+ */
+public class ThreadTest{
+
+    public static void main(String[] args) {
+        Thread01 thread01 = new Thread01();
+        thread01.start();
+        Thread thread02 = new Thread(new Thread02());
+        thread02.start();
+        FutureTask<String> task = new FutureTask<>(new Thread03());
+        Thread thread03 = new Thread(task);
+        thread03.start();
+    }
+}
+
+class Thread01 extends Thread{
+    @Override
+    public void run() {
+        System.out.println("线程1启动完毕");
+    }
+}
+
+class Thread02 implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("线程2启动完毕");
+    }
+}
+
+class Thread03 implements Callable<String>{
+
+    @Override
+    public String call() throws Exception {
+        System.out.println("线程3启动完毕");
+        return "成功";
+    }
+}
