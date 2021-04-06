a04061e110611c63cfc2d35b8faaaf195d07fc4c
cbjun <1656043947@qq.com>
2021-04-06 17:25:09 +0800

    创建线程的三种方法

--- spring-test/src/main/java/com/cbj/example/test/Test01.java
+++ spring-test/src/main/java/com/cbj/example/test/Test01.java
@@ -3,9 +3,9 @@ package com.cbj.example.test;
 //各种排序
 public class Test01 {
     public static void main(String[] args) {
-        int[] ary = {1,3,5,8,9,6,7,4,2};
+        int[] ary = {1, 3, 5, 8, 9, 6, 7, 4, 2};
         //冒泡排序
-         mppx(ary);
+        mppx(ary);
 
         //选择排序
         //xzpx(ary);
@@ -13,69 +13,80 @@ public class Test01 {
         //直接插入排序
 //        crpx(ary);
 
+        //菲波那切数列
+        System.out.println(fei(4));
     }
 
-    public static void mppx(int[] ary){
-        for(int i = 1; i<ary.length;i++){
-            for(int j = 0;j<ary.length - i;j++){
-                if(ary[j]>ary[j+1]){
+    public static void mppx(int[] ary) {
+        for (int i = 1; i < ary.length; i++) {
+            for (int j = 0; j < ary.length - i; j++) {
+                if (ary[j] > ary[j + 1]) {
                     int temp = ary[j];
-                    ary[j] = ary[j+1];
-                    ary[j+1] = temp;
+                    ary[j] = ary[j + 1];
+                    ary[j + 1] = temp;
                 }
             }
 
-            System.out.print("第"+(i)+"轮：");
-            for(int value:ary){
-                System.out.print(value+" ");
+            System.out.print("第" + (i) + "轮：");
+            for (int value : ary) {
+                System.out.print(value + " ");
             }
             System.out.println();
         }
 
-        System.out.print("冒泡排序："+" ");
-        for(int value:ary){
-            System.out.print(value+" ");
+        System.out.print("冒泡排序：" + " ");
+        for (int value : ary) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
 
-    public static void xzpx(int ary[]){
-        for(int i = 0;i<ary.length-1;i++){
-            for(int j = i+1;j<ary.length;j++){
-                if(ary[i] > ary[j]){
+    public static void xzpx(int ary[]) {
+        for (int i = 0; i < ary.length - 1; i++) {
+            for (int j = i + 1; j < ary.length; j++) {
+                if (ary[i] > ary[j]) {
                     int temp = ary[i];
                     ary[i] = ary[j];
                     ary[j] = temp;
                 }
             }
-            System.out.print("第"+(i+1)+"轮：");
-            for(int value:ary){
-                System.out.print(value+" ");
+            System.out.print("第" + (i + 1) + "轮：");
+            for (int value : ary) {
+                System.out.print(value + " ");
             }
             System.out.println();
         }
-        System.out.print("选择排序："+" ");
-        for(int value:ary){
-            System.out.print(value+" ");
+        System.out.print("选择排序：" + " ");
+        for (int value : ary) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
 
-    public static void crpx(int[] ary){
-        int j ;
-        for(int i = 1;i<ary.length;i++){
+    public static void crpx(int[] ary) {
+        int j;
+        for (int i = 1; i < ary.length; i++) {
             int temp = ary[i];
             j = i;
-            while(j>0 && temp<ary[j-1]){
-                ary[j] = ary[j-1];
+            while (j > 0 && temp < ary[j - 1]) {
+                ary[j] = ary[j - 1];
                 j--;
             }
             ary[j] = temp;
         }
-        System.out.print("插入排序："+" ");
-        for(int value:ary){
-            System.out.print(value+" ");
+        System.out.print("插入排序：" + " ");
+        for (int value : ary) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
+
+    public static int fei(int n){
+        if(n<3){
+            return 1;
+        }else{
+            return fei(n-1)+fei(n-2);
+        }
+    }
+
 }
